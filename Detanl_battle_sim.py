5b63064418edd3d716d5e862e711cd4cd92f46ac
sach-school <52386795+sach-school@users.noreply.github.com>
2019-09-10 13:13:32 +1200

    Attacks and End
    Updated the end screen, now includes users name and made it more efficient by using a list
    Added the user attacks and the villains; names, attacks, some damage numbers, and a few information bits
    ---------------------------------------------------------------------------------------------------
    Next steps:
    Add user and villain health
    Add the game play - start with how the user chooses attacks and how they can heal

--- Detanl_battle_sim.py
+++ Detanl_battle_sim.py
@@ -1,9 +1,6 @@
 # Detal_battle_sim.py
-# v0.03
 
-# Title Page
-
-# Modified: 29/07/2019
+# Modified: /07/2019
 # Created: 29/07/2019
 # By Chris Sa
 
@@ -58,44 +55,82 @@ def username():
             color.write("Please enter a 3 character user name", "COMMENT")
     return user
 
-def exit_game():
+def menu():
+    """List all the options for the user to do"""
+    print("""\n=====~=====~=====~=====~=====~=====~=====~=====~=====~=====~=====~=====~=====~==
+To select an option enter the letter assigned to it
+To Start the tutorial type: T
+To Start the game type: S
+To Quit the program type: Q
+=====~=====~=====~=====~=====~=====~=====~=====~=====~=====~=====~=====~=====~==
+""")
+    # Calling the error module to get a valid input from the user
+    choice = error_check(['t','s','q'], "--> ", "ERROR! Please enter a valid letter as listed above.", False)
+    return choice
+
+def attacks(user):
+    user_attacks = {"Floss Lasso": [[3,7,15], "Flossing helps..."],"Brush Brush": [[4,9,13], "Brushing helps..."],"See a Dentist": [[5,10,14], "If you your teeth hurt then you should see a dentist"]}
+    mrs_fizz = {"Dissolve": [[5]],"Spray": [[2]],"Acid Slap": [[4]], "Catch frase i think": "The Carbon Dioxied (The thing that makes them Fizz) in fizzy drinks can cause damage to your teeth because it is acidic"}
+
+def tutorial(user):
+    """Teach the user how to play the game"""
+    color.write("Welcome to ", "BULLETIN")
+    color.write("Dentist Fight Sim \n", "KEYWORD")
+    print("When you get into a battle you will have 3 attcaks to chose from, Floss Lasso, Brush Brush and See a Dentist")
+    
+
+def start(user):
+    """Start the game for the user"""
+    pass
+
+def game_over(user):
+    """Allow the user to decide whether they want to try again, return to the main menu or quit after a death"""
+    print("""\n=====~=====~=====~=====~=====~=====~=====~=====~=====~=====~=====~=====~=====~==
+To select an option enter the letter assigned to it
+To Try Again type: T
+To go to the Main menu type: M
+To Quit the program type: Q
+=====~=====~=====~=====~=====~=====~=====~=====~=====~=====~=====~=====~=====~==
+""")
+    # Calling the error module to get a valid input from the user
+    choice = error_check(['t','m','q'], "--> ", "ERROR! Please enter a valid letter as listed above.", False)
+    if choice == 't':
+        start(user)
+    elif choice == 'q':
+        exit_game()
+    else:
+        pass
+
+def exit_game(user):
+    print("ChrisPy would like to thank you, {} for taking the time to play my game. I hope to see you again soon".format(user))
+    time.sleep(2)
     for i in range (15):
         print("")
         time.sleep(.1)
-    print("    _______   _______ .__   __. .___________. __       _______.___________.     ")
-    time.sleep(.1)
-    print("   |       \ |   ____||  \ |  | |           ||  |     /       |           |     ")
-    time.sleep(.1)
-    print("   |  .--.  ||  |__   |   \|  | `---|  |----`|  |    |   (----`---|  |----`     ")
-    time.sleep(.1)
-    print("   |  |  |  ||   __|  |  . `  |     |  |     |  |     \   \       |  |          ")
-    time.sleep(.1)
-    print("   |  '--'  ||  |____ |  |\   |     |  |     |  | .----)   |      |  |        ")
-    time.sleep(.1)
-    print("   |_______/ |_______||__| \__|     |__|     |__| |_______/       |__|          ")
-    time.sleep(.1)
-    print("")
-    time.sleep(.1)
-    print("")
-    time.sleep(.1)
-    print(" _______  __    _______  __    __  .___________.        _______. __  .___  ___. ")
-    time.sleep(.1)
-    print("|   ____||  |  /  _____||  |  |  | |           |       /       ||  | |   \/   | ")
-    time.sleep(.1)
-    print("|  |__   |  | |  |  __  |  |__|  | `---|  |----`      |   (----`|  | |  \  /  | ")
-    time.sleep(.1)
-    print("|   __|  |  | |  | |_ | |   __   |     |  |            \   \    |  | |  |\/|  | ")
-    time.sleep(.1)
-    print("|  |     |  | |  |__| | |  |  |  |     |  |        .----)   |   |  | |  |  |  | ")
-    time.sleep(.1)
-    print("|__|     |__|  \______| |__|  |__|     |__|        |_______/    |__| |__|  |__| ")
+    end_screen = ["    _______   _______ .__   __. .___________. __       _______.___________.     ",
+                  "   |       \ |   ____||  \ |  | |           ||  |     /       |           |     ",
+                  "   |  .--.  ||  |__   |   \|  | `---|  |----`|  |    |   (----`---|  |----`     ",
+                  "   |  |  |  ||   __|  |  . `  |     |  |     |  |     \   \       |  |          ",
+                  "   |  '--'  ||  |____ |  |\   |     |  |     |  | .----)   |      |  |          ",
+                  "   |_______/ |_______||__| \__|     |__|     |__| |_______/       |__|          ",
+                  "", "",
+                  " _______  __    _______  __    __  .___________.        _______. __  .___  ___. ",
+                  "|   ____||  |  /  _____||  |  |  | |           |       /       ||  | |   \/   | ",
+                  "|  |__   |  | |  |  __  |  |__|  | `---|  |----`      |   (----`|  | |  \  /  | ",
+                  "|   __|  |  | |  | |_ | |   __   |     |  |            \   \    |  | |  |\/|  | ",
+                  "|  |     |  | |  |__| | |  |  |  |     |  |        .----)   |   |  | |  |  |  | ",
+                  "|__|     |__|  \______| |__|  |__|     |__|        |_______/    |__| |__|  |__| "
+                  ,""]
+
+    for i in range(len(end_screen)):
+        print(end_screen[i])
+        time.sleep(0.1)
+    
     for i in range(30):
         time.sleep(.1)
         print("")
     time.sleep(3)
     print("\n" * 100)
-    
-    
     
 
 def main():
@@ -105,21 +140,22 @@ def main():
     q = False
     
     while q == False:
-        choice = input("Choice = ")
+        choice = menu()
 
         # Check what choice the user inputed then call that function
 
         if choice == 't' or choice == "tutorial":
-            tutorial()
+            tutorial(user)
         elif choice == 's' or choice == "start":
             start(user)
         else:
             print("Thank you for plating my game. Goodbye")
-            exit_game()
+            exit_game(user)
             q = True
     
 
-main()
+if __name__ == '__main__':
+    main()
 
 
 
